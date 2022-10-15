5c14e90e5dc73941035a66ebeeeb63c17a8258b9
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-10-15 12:54:11 +0600

    add route to get all random user

--- index.js
+++ index.js
@@ -4,8 +4,30 @@ const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 app.use(express.json());
+const FS = require("fs");
 
 const port = process.env.PORT || 4000;
+
+/*===============================
+    All Routes and Handlers
+=================================*/
+
+app.get("/GET/user/all", (req, res) => {
+    FS.readFile("Data.json", "utf-8", (err, data) => {
+        if (err) {
+            res.send({
+                status: "failed",
+                message: err.message,
+            });
+        } else {
+            const result = JSON.parse(data);
+            res.send({
+                status: "success",
+                result,
+            });
+        }
+    });
+});
 
 app.get("/", (req, res) => {
     res.send("Server is Running");
